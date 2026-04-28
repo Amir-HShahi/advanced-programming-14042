e4bfb31bd78f86442c71c201c24a919cfbbe28e8
Amir-HShahi <amir13831107h@gmail.com>
2026-04-28 19:40:54 +0330

    add: session 2

---
+++ class-codes/session-02/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+    void main() {
+        String reversed = StringUtils.reverse("AAAA----BBBB");
+        System.out.println(reversed);
+    }
+}
